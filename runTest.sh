54864acce39d64cc6df39cd6793c6e73389a7987
sg33368 <101246276+sg33368@users.noreply.github.com>
2022-03-15 11:11:52 +0000

    2

--- runTest.sh
+++ runTest.sh
@@ -5,7 +5,7 @@ results=PerfTest_Results_$time
 mkdir ./$results
 mkdir ./HTMLTReport_$time
 echo "test starting"
-nohup sh apache-jmeter-5.4.3/apache-jmeter-5.4.3/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=csv -n -t ./$sciptName -f -l ./$projectName.jtl -e -o ./HTMLReport_$time
+nohup sh C:/Users/pa_sg33368/Downloads/apache-jmeter-5.4.3/apache-jmeter-5.4.3/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=csv -n -t ./$sciptName -f -l ./$projectName.jtl -e -o ./HTMLReport_$time
 ./$results/nohup.txt 2>&1
 sh apache-jmeter-5.4.3/apache-jmeter-5.4.3/bin/JmeterPluginsCMD.sh --generate.csv ./Aggregate_$time.csv --input-jtl ./$projectName.jtl --plugin-type AggregateReport
 mv ./HTMLReport_$time ./$results
--- runTest.sh.bak
+++ runTest.sh.bak
@@ -4,6 +4,7 @@ time=$(date "+%Y.%m.%d-%H.%M")
 results=PerfTest_Results_$time
 mkdir ./$results
 mkdir ./HTMLTReport_$time
+echo "test starting"
 nohup sh apache-jmeter-5.4.3/apache-jmeter-5.4.3/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=csv -n -t ./$sciptName -f -l ./$projectName.jtl -e -o ./HTMLReport_$time
 ./$results/nohup.txt 2>&1
 sh apache-jmeter-5.4.3/apache-jmeter-5.4.3/bin/JmeterPluginsCMD.sh --generate.csv ./Aggregate_$time.csv --input-jtl ./$projectName.jtl --plugin-type AggregateReport
