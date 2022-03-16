1f3d9f81eeae2d454e8492c9aff850ff09864956
sg33368 <101246276+sg33368@users.noreply.github.com>
2022-03-16 10:53:02 +0000

    12

--- runTest.sh
+++ runTest.sh
@@ -3,17 +3,16 @@ scriptName=$projectName.jmx
 time=$(date "+%Y.%m.%d-%H.%M")
 results=PerfTest_Results_$time
 mkdir ./$results
-mkdir ./HTMLTReport_$time
+mkdir ./HTMLReport_$time
 echo "test starting"
-nohup sh apache-jmeter-5.4.3/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=csv -n -t ./$scriptName -f -l ./$projectName.jtl -e -o ./HTMLReport_$time
-./$results/nohup.txt 2>&1
-sh apache-jmeter-5.4.3/apache-jmeter-5.4.3/bin/JmeterPluginsCMD.sh --generate.csv ./Aggregate_$time.csv --input-jtl ./$projectName.jtl --plugin-type AggregateReport
+nohup sh apache-jmeter-5.4.3/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=csv -n -t ./$scriptName -f -l ./$projectName.jtl -e -o ./HTMLReport_$time > ./$results/nohup.txt 2>&1
+sh apache-jmeter-5.4.3/bin/JMeterPluginsCMD.sh --generate-csv ./Aggregate_$time.csv --input-jtl ./$projectName.jtl --plugin-type AggregateReport
 mv ./HTMLReport_$time ./$results
 mv ./$projectName.jtl ./$results
 mv ./jmeter.log ./$results
 mv ./GrafanaResults.txt ./$results
 mv ./Aggregate_$time.csv ./$results
 mv ./$projectName.properties ./$results
-echo "test finished find the results in folder $results"
+echo "Test Finished. Find test results in folder $results"
 
 
